ca37e70214fa4713481a4ade97968a30dab81b1b
Khattab Omar <khattab.latif@baly.iq>
2026-02-26 12:43:34 +0300

    1

--- migrations/000003_create_attendance_table.up.sql
+++
@@ -1,9 +0,0 @@
-SET search_path TO crud_project;
-
-CREATE TABLE IF NOT EXISTS attendance (
-    id SERIAL PRIMARY KEY,
-    employee_id INTEGER NOT NULL,
-    date DATE NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE
-);
